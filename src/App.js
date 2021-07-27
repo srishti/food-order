e512afb4a845647966e39dcca61abe44e52178db
Srishti Gupta <mail@srishtigupta.in>
2021-07-27 21:07:23 +0530

    moved Header and Banner inside Layout folder and modified their CSS

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import React, { useState, useContext } from "react";
-import Header from "./components/Header/Header";
+import Header from "./components/Layout/Header";
+import Banner from "./components/Layout/Banner";
 import CartSummary from "./components/Cart/CartSummary";
-import Banner from "./components/UI/Banner";
 import Intro from "./components/Food/Intro";
 import FoodItems from "./components/Food/FoodItems";
 import FoodContext from "./context/food-context";
@@ -24,7 +24,7 @@ const App = () => {
       {isCartSummaryVisible && foodContext.selectedItems.length > 0 && (
         <CartSummary hideCartSummary={hideCartSummary} />
       )}
-      <Banner src="https://images.unsplash.com/photo-1517248135467-4c7edcad34c4?ixid=MnwxMjA3fDB8MHxzZWFyY2h8MjF8fHJlc3RhdXJhbnR8ZW58MHx8MHx8&ixlib=rb-1.2.1&w=1000&q=80" />
+      <Banner />
       <Intro />
       <FoodItems />
     </div>
--- src/components/Layout/Banner.js
+++ src/components/Layout/Banner.js
@@ -1,10 +1,11 @@
 import React from "react";
+import mealsImage from "../../assets/meals.jpeg";
 import styles from "./Banner.module.css";
 
-const Banner = (props) => {
+const Banner = () => {
   return (
     <div className={styles["banner"]}>
-      <img src={props.src} />
+      <img src={mealsImage} alt="Meals Banner" />
     </div>
   );
 };
